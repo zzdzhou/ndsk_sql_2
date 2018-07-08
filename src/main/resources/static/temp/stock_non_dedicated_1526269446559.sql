b9c0a6012e10c1d029dfa75c0a15c3ee3df13a92
zzdzhou <zzd16000@163.com>
2018-07-08 17:12:26 +0800

    20180708

---
+++ src/main/resources/static/temp/stock_non_dedicated_1526269446559.sql
@@ -0,0 +1,13 @@
+START TRANSACTION;
+
+INSERT INTO `stock_non_dedicated` (`REFERENCE`, `COUNTRY`, `BRAND`, `QUANTITY`, `NDSK_DELIVERY_LEAD_TIME_ID`) VALUES
+('Q9068670', 'CH', 'JL', 3, 154),
+('Q9028480', 'CH', 'JL', 3, 154),
+('Q9028471', 'CH', 'JL', 3, 154),
+('Q9028470', 'CH', 'JL', 1, 154),
+('Q9028180', 'CH', 'JL', 1, 154),
+('Q9028170', 'CH', 'JL', 3, 154),
+('Q9022450', 'CH', 'JL', 57, 154),
+('Q9038670', 'CH', 'JL', 35, 154),
+('Q8126420', 'CH', 'JL', 1, 154),
+('Q8108420', 'CH', 'JL', 5, 154);
